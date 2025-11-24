7d6ac3f0808a2a7b69ef3a677973272bbe161b37
Paul Manu <manup4915@gmail.com>
2025-11-24 10:06:59 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#EEDC82",
+        lightgrey: "#F2F2F2",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
